66e5ff3b75a82e71384192863d0da933a817e69d
miajimyu <38499336+miajimyu@users.noreply.github.com>
2020-02-12 00:08:51 +0900

    Add DiceIcon in HistoryScreen

--- lib/screen/history_screen.dart
+++ lib/screen/history_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:dice/model/history.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
@@ -8,22 +11,64 @@ class HistoryScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<History>(
       builder: (context, value, child) {
-        return ListView.separated(
-          separatorBuilder: (context, index) => Divider(
-            color: Colors.grey,
-          ),
-          itemCount: value.historys.length,
-          itemBuilder: (context, index) {
-            final i = value.historys.length - index - 1;
-            return ListItem(
-              result: value.historys[i].result,
-              name: value.historys[i].diceName,
-              results: value.historys[i].results,
-              time: value.historys[i].dateTime,
-            );
-          },
-        );
+        return value.historys.isEmpty
+            ? Center(child: DiceIcon())
+            : ListView.separated(
+                separatorBuilder: (context, index) => Divider(
+                  color: Colors.grey,
+                ),
+                itemCount: value.historys.length,
+                itemBuilder: (context, index) {
+                  final i = value.historys.length - index - 1;
+                  return ListItem(
+                    result: value.historys[i].result,
+                    name: value.historys[i].diceName,
+                    results: value.historys[i].results,
+                    time: value.historys[i].dateTime,
+                  );
+                },
+              );
       },
+    );
+  }
+}
+
+class DiceIcon extends StatelessWidget {
+  Icon _buildIcon() {
+    final result = Random().nextInt(6) + 1;
+    switch (result) {
+      case 1:
+        return Icon(FontAwesomeIcons.diceOne, size: 50);
+        break;
+      case 2:
+        return Icon(FontAwesomeIcons.diceTwo, size: 50);
+        break;
+      case 3:
+        return Icon(FontAwesomeIcons.diceThree, size: 50);
+        break;
+      case 4:
+        return Icon(FontAwesomeIcons.diceFour, size: 50);
+        break;
+      case 5:
+        return Icon(FontAwesomeIcons.diceFive, size: 50);
+        break;
+      case 6:
+        return Icon(FontAwesomeIcons.diceSix, size: 50);
+        break;
+      default:
+        return Icon(FontAwesomeIcons.diceD20, size: 50);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        _buildIcon(),
+        const SizedBox(height: 20),
+        Text('Empty', style: Theme.of(context).textTheme.display1),
+      ],
     );
   }
 }
