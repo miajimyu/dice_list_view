375220ec3b59d03184d11b23ac7b3a5898f9d906
miajimyu <miajimyu@users.noreply.github.com>
2020-02-29 12:16:19 +0000

    deploy: d79cc746eda257b53eb4aae90403fe92cea8e438

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,19 +1,19 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/assets/LICENSE": "e939f280462c2ef02b84993457906d48",
-"/assets/FontManifest.json": "18eda8e36dfa64f14878d07846d6e17f",
-"/assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "3ca122272cfac33efb09d0717efde2fa",
-"/assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "d21f791b837673851dd14f7c132ef32e",
-"/assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "bdd8d75eb9e6832ccd3117e06c51f0d3",
-"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets/AssetManifest.json": "801125fca546f506babac9af4ec061ba",
-"/index.html": "2aad5fd7f0591126b45c26016e7f2b2a",
-"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+  "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/main.dart.js": "11035975088a65c82d30544944e2dc5a",
-"/manifest.json": "8e156ffd282cbae221c4559236292670"
+"/manifest.json": "8e156ffd282cbae221c4559236292670",
+"/index.html": "2aad5fd7f0591126b45c26016e7f2b2a",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/assets/FontManifest.json": "18eda8e36dfa64f14878d07846d6e17f",
+"/assets/LICENSE": "e939f280462c2ef02b84993457906d48",
+"/assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "d21f791b837673851dd14f7c132ef32e",
+"/assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "3ca122272cfac33efb09d0717efde2fa",
+"/assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "bdd8d75eb9e6832ccd3117e06c51f0d3",
+"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"/assets/AssetManifest.json": "801125fca546f506babac9af4ec061ba"
 };
 
 self.addEventListener('activate', function (event) {
